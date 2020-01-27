a23ea0480eab5abd17963c3117e4efa8dbc68179
Your Name <you@example.com>
2020-01-27 19:17:30 +0100

    Working config

--- CUPS/Configuration.md
+++ CUPS/Configuration.md
@@ -11,3 +11,22 @@ sudo apt-get install libcups2-dev
 sudo apt install python3-dev
 
 sudo apt-get install cups samba
+
+
+In smb.conf set in [global] 
+rpc_server:spoolss = external
+rpc_daemon:spoolssd = fork
+load printers = no
+printing = bsd
+
+and add section
+[filip_printer]
+  path = /var/spool/samba
+  print command = /usr/bin/python3 /home/filip/Development/PPS/CUPS/monitor.py "%s" "%I" "%p" "%T"
+  comment = HP Shared printer
+  printable = yes
+  browsable = yes
+  writable = no
+  guest ok = yes
+
+and comment [printers] section
--- CUPS/monitor.py
+++ CUPS/monitor.py
@@ -1,14 +1,31 @@
 #!/bin/env python3
 import time
-
+import sys
+import subprocess
+import os
 
 def main():
+    FILE = sys.argv
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
 
+    import getpass
+    username = getpass.getuser()
+
     file = open("/var/spool/samba/log.txt", "w")
+    file.write(username)
     file.write(current_time)
+    for arg in FILE:
+        file.write('\n')
+        file.write(arg)
     file.close()
+    print_file = sys.argv[1]
+    file_to_print = "/var/spool/samba/" + print_file
+    print(file_to_print)
+    # subprocess.run(["lpr", "-P HP_LaserJet_P2055dn", file_to_print])
+
+    # Send to printer
+    # os.system("lpr -P HP_LaserJet_P2055dn " + file_to_print)
 
 
 if __name__ == '__main__':
